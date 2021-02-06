5e47704fe56cb920e50b0f61c0b27d534667cbb3
Graitavicius <grait.mindaugas@gmail.com>
2021-02-06 16:55:36 +0200

    created basic layout for the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { VideoComponent } from './video/video.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    VideoComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule
